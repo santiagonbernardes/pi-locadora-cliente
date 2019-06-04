8af9d6b7b9c70eeeed0e7d8d7a5e30b9d879a5de
Pablo Fellype <pablopslv@gmail.com>
2019-06-04 20:11:48 -0300

    Criação tela Funcionário

--- src/main/java/br/com/senaigo/locadora/main/Main.java
+++ src/main/java/br/com/senaigo/locadora/main/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, Exception {
             
             TelaLogon tela = new TelaLogon();
             tela.setVisible(true);
